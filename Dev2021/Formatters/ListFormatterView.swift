8cd4fbf2262a4e71ac536a189bd67b5f361157c5
yang_jinxin <yang_jinxin@linecorp.com>
2021-04-15 11:31:26 +0800

    format

--- Dev2021/Formatters/ListFormatterView.swift
+++ Dev2021/Formatters/ListFormatterView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct ListFormatterView: View {
+
+    // In English: "Apples, Sugar, Flour, and Butter"
+    let englishList = ListFormatter.localizedString(
+        byJoining: ["Apples", "Sugar", "Flour", "Butter"]
+    )
+
+
+    // In Swedish: // "Äpplen, Socker, Mjöl och Smör"
+    let SwedishList = ListFormatter.localizedString(
+        byJoining: ["Äpplen", "Socker", "Mjöl", "Smör"]
+    )
+
     var body: some View {
-        Text("ListFormatter").padding()
+        //Text("ListFormatter").padding(.bottom, 10)
+
+        VStack {
+            Text("ListFormatter").padding(.bottom, 20)
+
+            Text(englishList).padding(.bottom, 20)
+            Text(SwedishList).padding(.bottom, 20)
+        }
     }
 }
 
